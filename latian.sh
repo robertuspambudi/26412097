c97b3114ca3571de432e52522796bdc03f720274
robertus <m26412097@john.petra.ac.id>
2016-10-20 09:06:48 +0700

    latian coding

---
+++ latian.sh
@@ -0,0 +1,15 @@
+t status
+   65  cd 26412097
+   66  status
+   67  git status
+   68  git add
+   69  touch coba.sh
+   70  git add coba.sh
+   71  git commit -m"mencoba commit"
+   72  git commit -m "mencoba commit"
+   73  git push
+   74  robertuspambudi
+   75  git push
+   76  touch latian.sh
+   77  history
+
